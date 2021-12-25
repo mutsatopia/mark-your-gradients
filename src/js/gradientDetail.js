9b67cbae57c5ad4357de5c7637e8e8cb0d024a92
Jetty2020 <kmclo@naver.com>
2021-12-25 16:04:14 +0900

    [feat] 클립보드에 문자열 복사 기능 구현

--- src/js/gradientDetail.js
+++ src/js/gradientDetail.js
@@ -10,3 +10,15 @@ openBtn.addEventListener('click', () => {
   modalCon.classList.remove('dis-none');
   openBtn.classList.add('dis-none');
 });
+
+const modalBtn = document.querySelector(".modal-btn");
+const content = document.querySelector(".modal-content")
+
+modalBtn.addEventListener('click', () => {
+  const tempElem = document.createElement('textarea');
+  tempElem.value = '복사 성공';  
+  document.body.appendChild(tempElem);
+
+  tempElem.select();
+  document.execCommand('copy');
+})
