e952859db6884e558b849d913bf84514769a6d72
yeeun_An <an446@naver.com>
2021-12-30 17:47:33 +0900

    [fix] main.js - color출력구현 수정

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,44 @@
 // list-miniboxli backgroundColor 지정하기
-const colorMiniArray = ["#D0637C", "#F27F0C", "#F7AD19", "yellow", "#B3DBD8", "#288994", "#0196C1", "#697A98", "black"]
-
+const colorMiniArray = [
+  '#D0637C',
+  '#F27F0C',
+  '#F7AD19',
+  'yellow',
+  '#B3DBD8',
+  '#288994',
+  '#0196C1',
+  '#697A98',
+  'black',
+];
 const colorMiniBtn = document.querySelectorAll('.list-minibox li button');
+const colorMainBox = document.querySelectorAll('.list-mainbox li');
 
-let i =0;
-while ( i < colorMiniArray.length){
-    colorMiniBtn.forEach(el => {
-        // console.log(`${colorMiniArray[i]}`);
-        el.style.backgroundColor = `${colorMiniArray[i]}`;
-        i+=1;
-    })
+let i = 0;
+while (i < colorMiniArray.length) {
+  colorMiniBtn.forEach((el) => {
+    // console.log(`${colorMiniArray[i]}`);
+    el.style.backgroundColor = `${colorMiniArray[i]}`;
+    i += 1;
+  });
 }
-
-
 
 //main color box
-
-const maincolorLi = document.querySelectorAll('.list-mainbox li')
-const direction = ['top', 'right', 'bottom', 'left'];
-
-for (const minibtn of colorMiniBtn){
-    minibtn.addEventListener("click", printColor());
-}
-function printColor() {
-    for (const buttonNum in colorMiniBtn){
-        
-        let selectColors = data.filter(i => i.id === (buttonNum*9)).map(i => i.colors);
-        console.log(selectColors);
-        let color1 = selectColors[0];
-        let color2 = selectColors[1];
-    }
-}
+colorMiniBtn.forEach((button, index) => {
+  button.addEventListener('click', (event) => {
+    let start_id = index * 9;
+    let end_id = index * 9 + 8;
+    
+    const selecColors = data.filter((s) => s.id === index*9).map((s) => s.colors);
+    const color1 = selecColors[0][0];
+    const color2 = selecColors[0][1];
+    // const b_index =  % 9;
+    // const selecBox = colorMainBox[b_index];
+//   console.log(colorMainBox[b_index].style.background='red');
+    console.log(color1, color2)
+    // selecBox.style.background = `linear-gradient( to-right ${color1},${color2})`;
+    
+  });
+});
 
 // for (const button of colorMiniBtn){
 //     button.addEventListener('click', function(event){
@@ -48,17 +56,15 @@ function printColor() {
 //     })
 // }
 
-
 //mainbox - txt
-const txt =document.querySelector('.box-txt');
-for (const box of maincolorLi){
-    box.addEventListener('click', () => {
-        if (!txt.contains('hide')){
-            txt.classList.add('hide');
-        }else{
-            txt.classList.remove('hide')
-        }
-    })
-    
-}
+// const txt =document.querySelector('.box-txt');
+// for (const box of maincolorLi){
+//     box.addEventListener('click', () => {
+//         if (!txt.contains('hide')){
+//             txt.classList.add('hide');
+//         }else{
+//             txt.classList.remove('hide')
+//         }
+//     })
 
+// }
