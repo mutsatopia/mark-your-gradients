1f4226c6f23532cf9f0905dc239bd955df26297e
skgml0 <hnh2714@gmail.com>
2022-01-02 02:30:56 +0900

    [fix] 조건문 실행버그 수정

--- src/js/Bookmark.js
+++ src/js/Bookmark.js
@@ -11,20 +11,18 @@ function local_store() {
   // arrValue에 이름이 있다면? - ! > 없을경우 참
   // localStorage.clear();
   if (
-    !(gradientData.name in Object.keys(arrValue)) &&
+    !Object.keys(arrValue).includes(gradientData.name) &&
     Object.keys(arrValue).length < 5
   ) {
     arrValue[gradientData.name] = mainColor.style.background;
     console.log(arrValue);
     localStorage.setItem(nameKey, JSON.stringify(arrValue));
     console.log('값 추가 첫번째 경우');
-    console.log(gradientData.name in Object.keys(arrValue));
-    console.log(Object.keys(arrValue));
-    console.log(gradientData.name);
+    alert('color 저장을 완료했습니다!');
   }
   // arrvalue에 이름이 없다면 , 그리고 길이가 5 이상이면 맨 앞을 지운다.
   else if (
-    !(gradientData.name in Object.keys(arrValue)) &&
+    !Object.keys(arrValue).includes(gradientData.name) &&
     Object.keys(arrValue).length >= 5
   ) {
     // 0번째 key값을 가지고 있는 것 localStorage에서 키값 지우기.
@@ -35,8 +33,8 @@ function local_store() {
     arrValue[gradientData.name] = mainColor.style.background;
     localStorage.setItem(nameKey, JSON.stringify(arrValue));
     console.log('값 추가 두번째 경우');
+    alert('5개 칸이 꽉 차, 한 개를 삭제하고 저장했습니다');
   } else {
-    console.log('값이 겹치는 경우');
+    alert('이미 저장되어있습니다.');
   }
-  alert('저장완료');
 }
